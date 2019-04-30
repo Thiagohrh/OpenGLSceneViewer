a32c06f046894ebb53e64c9488e6c847a34ef62b
Thiago Henrique <thiagohrh@gmail.com>
2019-04-29 23:07:50 -0300

    Updated with more tools to manipulate the model.
    Yeah.

--- GLFWTriangleTest - Solution/Exercise4/Main4.cpp
+++ GLFWTriangleTest - Solution/Exercise4/Main4.cpp
@@ -11,6 +11,7 @@
 
 
 #include <iostream>
+#include <vector>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
@@ -30,6 +31,40 @@ bool firstMouse = true;
 // timing
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
+
+struct Transformations
+{
+	bool rotate_clock = false;
+	bool rotate_counter_clock = false;
+	bool rotate_x_up = false;
+	bool rotate_x_down = false;
+	bool rotate_z_positive = false;
+	bool rotate_z_negative = false;
+
+	bool scale_up = false;
+	bool scale_down = false;
+
+	bool translate_right = false;
+	bool translate_left = false;
+	bool translate_back = false;
+	bool translate_forward = false;
+	bool translate_up = false;
+	bool translate_down = false;
+};
+
+
+enum transformation_mode { ROTATING, TRANSLATING, SCALING };
+int currentmode = 0;
+
+glm::mat4 model;
+glm::mat4 model2;
+
+glm::vec3 modelscale(.2f, .2f, .2f);
+
+std::vector<glm::mat4> all_models;
+int current_model = 0;
+
+bool changingModel = false;
 
 int main()
 {
@@ -87,6 +122,17 @@ int main()
 
 	// render loop
 	// -----------
+	model = glm::mat4(1.0f);
+	model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f)); // translate it down so it's at the center of the scene
+	model = glm::scale(model, modelscale);	// it's a bit too big for our scene, so scale it down
+
+	model2 = glm::mat4(1.0f);
+	model2 = glm::translate(model2, glm::vec3(1.0f, -1.75f, 0.0f)); // translate it down so it's at the center of the scene
+	model2 = glm::scale(model2, modelscale);	// it's a bit too big for our scene, so scale it down
+
+	all_models.push_back(model);
+	all_models.push_back(model2);
+
 	while (!glfwWindowShouldClose(window))
 	{
 		// per-frame time logic
@@ -114,11 +160,13 @@ int main()
 		ourShader.setMat4("view", view);
 
 		// render the loaded model
-		glm::mat4 model = glm::mat4(1.0f);
-		model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f)); // translate it down so it's at the center of the scene
-		model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));	// it's a bit too big for our scene, so scale it down
-		ourShader.setMat4("model", model);
-		ourModel.Draw(ourShader);
+
+		for (int i = 0; i < all_models.size(); i++)
+		{
+			ourShader.setMat4("model", all_models[i]);
+			ourModel.Draw(ourShader);
+			
+		}
 
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
@@ -133,11 +181,58 @@ int main()
 	return 0;
 }
 
+void SwitchMode(int _mode_choice)
+{
+	switch (_mode_choice)
+	{
+	case 0:
+		currentmode = ROTATING;
+		break;
+	case 1:
+		currentmode = TRANSLATING;
+		break;
+	case 2:
+		currentmode = SCALING;
+		break;
+	default:
+		break;
+	}
+	cout << currentmode << endl;
+}
+
+void SwitchModel() {
+	if (!changingModel)
+	{
+		if (current_model == 0) current_model = 1;
+		else current_model = 0;
+		changingModel = true;
+	}
+}
+
+
+glm::mat4 rotate_model(float rx, float ry, float rz, float _angle)
+{
+	return glm::rotate(all_models[current_model], glm::radians(_angle), glm::vec3(rx, ry, rz));
+}
+glm::mat4 translate_model(float tx, float ty, float tz)
+{
+	glm::vec3 pos(tx, ty, tz);
+	return glm::translate(all_models[current_model], pos);
+}
+glm::mat4 scale_model(float s)
+{
+
+	glm::vec3 modelscale(s, s, s);
+
+	return glm::scale(all_models[current_model], modelscale);
+}
+
+
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow *window)
 {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE))
 		glfwSetWindowShouldClose(window, true);
 
 	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
@@ -148,7 +243,146 @@ void processInput(GLFWwindow *window)
 		camera.ProcessKeyboard(LEFT, deltaTime);
 	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
 		camera.ProcessKeyboard(RIGHT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_TAB) == GLFW_PRESS)
+		SwitchModel();
+	if (glfwGetKey(window, GLFW_KEY_TAB) == GLFW_RELEASE)
+		changingModel = false;
+	if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS) {
+		SwitchMode(0);
+		//Makes the model rotate counter cockwise
+	}
+	if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS) {
+		SwitchMode(1);
+		//Makes the model rotate counter cockwise
+	}
+	if (glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS) {
+		SwitchMode(2);
+		//Makes the model rotate counter cockwise
+	}
+	if (glfwGetKey(window, GLFW_KEY_J) == GLFW_PRESS) {
+		//Makes the model rotate counter cockwise
+		float angle;
+		switch (currentmode)
+		{
+		case ROTATING:
+			//apply_transformation(model);
+			angle = 0.45;
+			all_models[current_model] = rotate_model(0.0f, -0.5f, 0.0f, angle);
+			break;
+		case TRANSLATING:
+			//apply_transformation(model);
+			all_models[current_model] = translate_model(-1.0f, 0.0f, 0.0f);
+			break;
+		default:
+			break;
+		}
+	}
+	if (glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS) {
+		//Makes the model rotate counter cockwise
+		float angle;
+		switch (currentmode)
+		{
+		case ROTATING:
+			//apply_transformation(model);
+			angle = 0.45;
+			all_models[current_model] = rotate_model(0.0f,0.5f,0.0f,angle);
+			break;
+		case TRANSLATING:
+			//apply_transformation(model);
+			all_models[current_model] = translate_model(1.0f, 0.0f, 0.0f);
+			break;
+		default:
+			break;
+		}
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS) {
+		//Makes the model rotate counter cockwise
+		float angle;
+		switch (currentmode)
+		{
+		case ROTATING:
+			//apply_transformation(model);
+			angle = 0.45;
+			all_models[current_model] = rotate_model(0.5f, 0.0f, 0.0f, angle);
+			break;
+		case TRANSLATING:
+			//apply_transformation(model);
+			angle = 0.45;
+			all_models[current_model] = translate_model(0.0f, -0.5f, 0.0f);
+			break;
+		default:
+			break;
+		}
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_K) == GLFW_PRESS) {
+		//Makes the model rotate counter cockwise
+		float angle;
+		switch (currentmode)
+		{
+		case ROTATING:
+			//apply_transformation(model);
+			angle = 0.45;
+			all_models[current_model] = rotate_model(-0.5f, 0.0f, 0.0f, angle);
+			break;
+		case TRANSLATING:
+			//apply_transformation(model);
+			angle = 0.45;
+			all_models[current_model] = translate_model(0.0f, 0.5f, 0.0f);
+			break;
+		default:
+			break;
+		}
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_U) == GLFW_PRESS) {
+		//Makes the model rotate counter cockwise
+		float angle;
+		switch (currentmode)
+		{
+		case ROTATING:
+			//apply_transformation(model);
+			angle = 0.45;
+			all_models[current_model] = rotate_model(0.0f, 0.0f, 0.5f, angle);
+			break;
+		case TRANSLATING:
+			//apply_transformation(model);
+			angle = 0.45;
+			all_models[current_model] = translate_model(0.0f, 0.0f, 0.5f);
+			break;
+		case SCALING:
+			all_models[current_model] = scale_model(.999f);
+			break;
+		default:
+			break;
+		}
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_O) == GLFW_PRESS) {
+		//Makes the model rotate counter cockwise
+		float angle;
+		switch (currentmode)
+		{
+		case ROTATING:
+			//apply_transformation(model);
+			angle = 0.45;
+			all_models[current_model] = rotate_model(0.0f, 0.0f, -0.5f, angle);
+			break;
+		case TRANSLATING:
+			//apply_transformation(model);
+			angle = 0.45;
+			all_models[current_model] = translate_model(0.0f, 0.0f, -0.5f);
+			break;
+		case SCALING:
+			all_models[current_model] = scale_model(1.001f);
+			break;
+		default:
+			break;
+		}
+	}
 }
+
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
 // ---------------------------------------------------------------------------------------------
@@ -185,4 +419,3 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
 	camera.ProcessMouseScroll(yoffset);
 }
-
