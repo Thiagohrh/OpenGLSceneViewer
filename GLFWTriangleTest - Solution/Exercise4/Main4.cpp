1c199ac346babe99719ed23ffe8d8a7ffb46b15f
Thiago Henrique <thiagohrh@gmail.com>
2019-06-16 02:25:05 -0300

    Checkpoint to Lights example
    Following OpenGL code on page: https://learnopengl.com/Lighting/Materials

--- GLFWTriangleTest - Solution/Exercise4/Main4.cpp
+++ GLFWTriangleTest - Solution/Exercise4/Main4.cpp
@@ -9,7 +9,6 @@
 #include "camera.h"
 #include "Model.h"
 
-
 #include <iostream>
 #include <vector>
 
@@ -32,39 +31,8 @@ bool firstMouse = true;
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
-struct Transformations
-{
-	bool rotate_clock = false;
-	bool rotate_counter_clock = false;
-	bool rotate_x_up = false;
-	bool rotate_x_down = false;
-	bool rotate_z_positive = false;
-	bool rotate_z_negative = false;
-
-	bool scale_up = false;
-	bool scale_down = false;
-
-	bool translate_right = false;
-	bool translate_left = false;
-	bool translate_back = false;
-	bool translate_forward = false;
-	bool translate_up = false;
-	bool translate_down = false;
-};
-
-
-enum transformation_mode { ROTATING, TRANSLATING, SCALING };
-int currentmode = 0;
-
-glm::mat4 model;
-glm::mat4 model2;
-
-glm::vec3 modelscale(.2f, .2f, .2f);
-
-std::vector<glm::mat4> all_models;
-int current_model = 0;
-
-bool changingModel = false;
+// lighting
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 
 int main()
 {
@@ -108,31 +76,87 @@ int main()
 	// -----------------------------
 	glEnable(GL_DEPTH_TEST);
 
-	// build and compile shaders
-	// -------------------------
-	Shader ourShader("shaders/model_loading.vs", "shaders/model_loading.fs");
+	// build and compile our shader zprogram
+	// ------------------------------------
+	Shader lightingShader("shaders/basic_lighting.vs", "shaders/basic_lighting.fs");
+	Shader lampShader("shaders/lamp.vs", "shaders/lamp.fs");
 
-	// load models
-	// -----------
-	Model ourModel("models/nanosuit/nanosuit.obj");
+	// set up vertex data (and buffer(s)) and configure vertex attributes
+	// ------------------------------------------------------------------
+	float vertices[] = {
+		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+
+		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+
+		0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+		0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+		0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+		0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+		0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+		0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+		0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+		0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+		0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+		0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+		0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+		0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
+	};
+	// first, configure the cube's VAO (and VBO)
+	unsigned int VBO, cubeVAO;
+	glGenVertexArrays(1, &cubeVAO);
+	glGenBuffers(1, &VBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindVertexArray(cubeVAO);
+
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// normal attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 
-	// draw in wireframe
-	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	// second, configure the light's VAO (VBO stays the same; the vertices are the same for the light object which is also a 3D cube)
+	unsigned int lightVAO;
+	glGenVertexArrays(1, &lightVAO);
+	glBindVertexArray(lightVAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// note that we update the lamp's position attribute's stride to reflect the updated buffer data
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
 
 	// render loop
 	// -----------
-	model = glm::mat4(1.0f);
-	model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f)); // translate it down so it's at the center of the scene
-	model = glm::scale(model, modelscale);	// it's a bit too big for our scene, so scale it down
-
-	model2 = glm::mat4(1.0f);
-	model2 = glm::translate(model2, glm::vec3(1.0f, -1.75f, 0.0f)); // translate it down so it's at the center of the scene
-	model2 = glm::scale(model2, modelscale);	// it's a bit too big for our scene, so scale it down
-
-	all_models.push_back(model);
-	all_models.push_back(model2);
-
 	while (!glfwWindowShouldClose(window))
 	{
 		// per-frame time logic
@@ -147,26 +171,42 @@ int main()
 
 		// render
 		// ------
-		glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		// don't forget to enable shader before setting uniforms
-		ourShader.use();
+		// be sure to activate shader when setting uniforms/drawing objects
+		lightingShader.use();
+		lightingShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
+		lightingShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
+		lightingShader.setVec3("lightPos", lightPos);
+		lightingShader.setVec3("viewPos", camera.Position);
 
 		// view/projection transformations
 		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 		glm::mat4 view = camera.GetViewMatrix();
-		ourShader.setMat4("projection", projection);
-		ourShader.setMat4("view", view);
+		lightingShader.setMat4("projection", projection);
+		lightingShader.setMat4("view", view);
 
-		// render the loaded model
+		// world transformation
+		glm::mat4 model = glm::mat4(1.0f);
+		lightingShader.setMat4("model", model);
 
-		for (int i = 0; i < all_models.size(); i++)
-		{
-			ourShader.setMat4("model", all_models[i]);
-			ourModel.Draw(ourShader);
-			
-		}
+		// render the cube
+		glBindVertexArray(cubeVAO);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+
+		// also draw the lamp object
+		lampShader.use();
+		lampShader.setMat4("projection", projection);
+		lampShader.setMat4("view", view);
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, lightPos);
+		model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
+		lampShader.setMat4("model", model);
+
+		glBindVertexArray(lightVAO);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
@@ -175,187 +215,34 @@ int main()
 		glfwPollEvents();
 	}
 
+	// optional: de-allocate all resources once they've outlived their purpose:
+	// ------------------------------------------------------------------------
+	glDeleteVertexArrays(1, &cubeVAO);
+	glDeleteVertexArrays(1, &lightVAO);
+	glDeleteBuffers(1, &VBO);
+
 	// glfw: terminate, clearing all previously allocated GLFW resources.
 	// ------------------------------------------------------------------
 	glfwTerminate();
 	return 0;
 }
 
-void SwitchMode(int _mode_choice)
-{
-	string output = "";
-	switch (_mode_choice)
-	{
-	case 0:
-		currentmode = ROTATING; 
-		output = "ROTATING";
-		break;
-	case 1:
-		currentmode = TRANSLATING;
-		output = "TRANSLATING";
-		break;
-	case 2:
-		currentmode = SCALING;
-		output = "SCALING";
-		break;
-	default:
-		break;
-	}
-	cout << "Switching transforming mode to " << output << endl;
-}
-
-void SwitchModel() {
-	if (!changingModel)
-	{
-		if (current_model == 0) current_model = 1;
-		else current_model = 0;
-		changingModel = true;
-	}
-}
-
-
-glm::mat4 rotate_model(float rx, float ry, float rz, float _angle)
-{
-	return glm::rotate(all_models[current_model], glm::radians(_angle), glm::vec3(rx, ry, rz));
-}
-glm::mat4 translate_model(float tx, float ty, float tz)
-{
-	glm::vec3 pos(tx, ty, tz);
-	return glm::translate(all_models[current_model], pos);
-}
-glm::mat4 scale_model(float s)
-{
-
-	glm::vec3 modelscale(s, s, s);
-
-	return glm::scale(all_models[current_model], modelscale);
-}
-
-
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow *window)
 {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE))
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
 
-	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) // W Moves forward
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
 		camera.ProcessKeyboard(FORWARD, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) // S Moves backward
-		camera.ProcessKeyboard(BACKWARD, deltaTime); 
-	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) // A Moves leftward
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
 		camera.ProcessKeyboard(LEFT, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) // D Moves rightward
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
 		camera.ProcessKeyboard(RIGHT, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_TAB) == GLFW_PRESS) // TAB Switches which model is being transformed
-		SwitchModel();
-	if (glfwGetKey(window, GLFW_KEY_TAB) == GLFW_RELEASE) // TAB Switches which model is being transformed
-		changingModel = false;
-
-	if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS) { SwitchMode(0); } // 1 Switches transformation mode to ROTATING
-	if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS) { SwitchMode(1); } // 2 Switches transformation mode to TRANSLATING
-	if (glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS) { SwitchMode(2); } // 3 Switches transformation mode to SCALING
-
-	if (glfwGetKey(window, GLFW_KEY_4) == GLFW_PRESS) { camera.CameraViewpoint(glm::vec3(0.0f, 0.0f, 5.0f), -90.0f, 0.0f); } // 4 Go to view: FRONT
-	if (glfwGetKey(window, GLFW_KEY_5) == GLFW_PRESS) { camera.CameraViewpoint(glm::vec3(0.0f, 0.0f, -5.0f), 90.0f, 0.0f); } // 5 Go to view: BACK
-	if (glfwGetKey(window, GLFW_KEY_6) == GLFW_PRESS) { camera.CameraViewpoint(glm::vec3(-5.0f, 0.0f, 0.0f), 0.0f, 0.0f); } // 6 Go to view: LEFT
-	if (glfwGetKey(window, GLFW_KEY_7) == GLFW_PRESS) { camera.CameraViewpoint(glm::vec3(5.0f, 0.0f, 0.0f), 180.0f, 0.0f); } // 7 Go to view: RIGHT
-	if (glfwGetKey(window, GLFW_KEY_8) == GLFW_PRESS) { camera.CameraViewpoint(glm::vec3(0.0f, 5.0f, 0.0f), -90.0f, -90.0f); } // 8 Go to view: ABOVE
-	if (glfwGetKey(window, GLFW_KEY_9) == GLFW_PRESS) { camera.CameraViewpoint(glm::vec3(0.0f, -5.0f, 0.0f), -90.0f, 90.0f); } // 9 Go to view: BELOW
-	if (glfwGetKey(window, GLFW_KEY_0) == GLFW_PRESS) { camera.CameraViewpoint(glm::vec3(0.0f, 0.0f, 0.0f), -90.0f, 0.0f); } // 0 Go to view: ORIGIN
-
-	float angle = .45f;
-
-	if (glfwGetKey(window, GLFW_KEY_J) == GLFW_PRESS) {
-		switch (currentmode)
-		{
-		case ROTATING:
-			all_models[current_model] = rotate_model(0.0f, -0.5f, 0.0f, angle);
-			break;
-		case TRANSLATING:
-			all_models[current_model] = translate_model(-1.0f, 0.0f, 0.0f);
-			break;
-		default:
-			break;
-		}
-	}
-	if (glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS) {
-		switch (currentmode)
-		{
-		case ROTATING:
-			all_models[current_model] = rotate_model(0.0f,0.5f,0.0f,angle);
-			break;
-		case TRANSLATING:
-			all_models[current_model] = translate_model(1.0f, 0.0f, 0.0f);
-			break;
-		default:
-			break;
-		}
-	}
-
-	if (glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS) {
-		switch (currentmode)
-		{
-		case ROTATING:
-			all_models[current_model] = rotate_model(0.5f, 0.0f, 0.0f, angle);
-			break;
-		case TRANSLATING:
-			all_models[current_model] = translate_model(0.0f, -0.5f, 0.0f);
-			break;
-		default:
-			break;
-		}
-	}
-
-	if (glfwGetKey(window, GLFW_KEY_K) == GLFW_PRESS) {
-		switch (currentmode)
-		{
-		case ROTATING:
-			all_models[current_model] = rotate_model(-0.5f, 0.0f, 0.0f, angle);
-			break;
-		case TRANSLATING:
-			all_models[current_model] = translate_model(0.0f, 0.5f, 0.0f);
-			break;
-		default:
-			break;
-		}
-	}
-
-	if (glfwGetKey(window, GLFW_KEY_U) == GLFW_PRESS) {
-		switch (currentmode)
-		{
-		case ROTATING:
-			all_models[current_model] = rotate_model(0.0f, 0.0f, 0.5f, angle);
-			break;
-		case TRANSLATING:
-			all_models[current_model] = translate_model(0.0f, 0.0f, 0.5f);
-			break;
-		case SCALING:
-			all_models[current_model] = scale_model(.999f);
-			break;
-		default:
-			break;
-		}
-	}
-
-	if (glfwGetKey(window, GLFW_KEY_O) == GLFW_PRESS) {
-		switch (currentmode)
-		{
-		case ROTATING:
-			all_models[current_model] = rotate_model(0.0f, 0.0f, -0.5f, angle);
-			break;
-		case TRANSLATING:
-			all_models[current_model] = translate_model(0.0f, 0.0f, -0.5f);
-			break;
-		case SCALING:
-			all_models[current_model] = scale_model(1.001f);
-			break;
-		default:
-			break;
-		}
-	}
 }
-
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
 // ---------------------------------------------------------------------------------------------
@@ -365,6 +252,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 	// height will be significantly larger than specified on retina displays.
 	glViewport(0, 0, width, height);
 }
+
 
 // glfw: whenever the mouse moves, this callback is called
 // -------------------------------------------------------
